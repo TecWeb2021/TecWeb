9ff6fd0c695b75dce5230f58c83a520a4c927524
Federico <federicochiarello99@gmail.com>
2021-01-21 13:14:45 +0100

    Head, searchbar, myFunction
    Modificati header. uniformata searchbar, rimossa MyFunction da html

--- checkForm.js
+++ checkForm.js
@@ -110,3 +110,14 @@ function caricamento() {
     handleClick();
     carica_placeholder();
 }
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("menu");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
