99beb354a1c5f746b6adaffb7d725a7acbe3fb0f
Federico <federicochiarello99@gmail.com>
2021-01-24 05:47:21 +0100

    finiti i controlli js

--- checkForm.js
+++ checkForm.js
@@ -14,14 +14,15 @@ var dettagli_form = {
 
     "titolo": [/^([\w\s\'\,\.\"]){10,40}$/, "Inserire il titolo della notizia"],
     "testo": [/.{25,}/, "Inserire il testo della notizia"],
-
     "immagine": [/./, "Nessun file selezionato"],
 
     "nomeUtente": [/^([\w]){5,15}$/, "Inserire il nome utente"],
     "password": [ /^(?=.*[0-9])(?=.*[a-z])[a-zA-Z0-9!.@#$%^&*]{8,16}$/, "Inserire la password"],
     "repeatpassword": [ /^(?=.*[0-9])(?=.*[a-z])[a-zA-Z0-9!.@#$%^&*]{8,16}$/, "Le password non combaciano"],
-    "email": [/^\w+(\.?\w+)*@\w+(\.?\w+)*(\.\w{2,3})+$/, "Inserire la mail"]
+    "email": [/^\w{2}\w*(\.?\w+)*@\w{2}\w*(\.?\w+)*(\.\w{2,3})+$/, "Inserire la mail"]
 };
+
+/******************* Script dalle pagine contenenti form *******************/
 
 function mostraMessaggio(input, stato) {
     var elemento = document.createElement("p");
@@ -41,8 +42,7 @@ function mostraMessaggio(input, stato) {
 }
 
 function validazioneCampo(input) {
-
-    // controlla se e' gia' presente messaggio d'errore e lo rimuove
+    // controlla se e' gia' presente il messaggio d'errore e lo rimuove
     var parent = input.parentNode;
     if (parent.children.length == 2) {
         parent.removeChild(parent.children[1]);
@@ -51,13 +51,13 @@ function validazioneCampo(input) {
     var regex = dettagli_form[input.id][0];
     var text = input.value;
 
+    // 
     if(input.id == "immagine") {
         if (input.files.length == 0)
             return mostraMessaggio(input,false);
         else
             return mostraMessaggio(input,true);
     }
-
     if (input.id == "data") {
         var data = Date.parse(input.value);
         if (data > Date.UTC(1947) && data < Date.UTC(2077))
@@ -65,35 +65,24 @@ function validazioneCampo(input) {
         else 
             return mostraMessaggio(input, false);
     }
+    if(input.id == "repeatpassword")
+        return checkPassword();
 
-    if(input.id == "repeatpassword") {
-        var pw = document.getElementById("password").value;
-        if(pw) {
-            if (pw == document.getElementById("repeatpassword").value)
-                return mostraMessaggio(input,true);
-            else
-                return mostraMessaggio(input,false);
-        }
-    }
 
-    // se il campo input e' opzionale e vuoto non effettua il controllo
+    // se il campo input e' opzionale e vuoto non manda nessun messaggio
     if (input.className === "opzionale" && !text) {
         return true;
     }
 
-    // se il contenuto e' non vuoto e non rispetta l'espr. regolare mosto errore
-    if (text.search(regex) != 0) {
-        // -1 se non trova il match, 0 se lo trova, k se trova il match dalla posizione k
+    // se il contenuto non rispetta l'espr. regolare mosta l'errore
+    if (text.search(regex) != 0)
         return mostraMessaggio(input, false);
-    } else {
-        // se input è corretto corretto
+    else
         return mostraMessaggio(input, true);
-    }
 }
 
 // Controllo dell'input delle form
 function validateForm() {
-
     var corretto = true;
     for (var key in dettagli_form) {
         var input = document.getElementById(key);
@@ -102,34 +91,86 @@ function validateForm() {
             corretto = corretto && risultato;
         }
     }
+    var spuntate = checkChecked();
+    corretto = corretto && spuntate;
     return corretto;
+}
+
+function mostraMessaggioCheckbox(sezioni) {
+    var corretto = true;
+    for(var key in sezioni) {
+        sezione = document.getElementById(spuntabili[key][0]);
+        if(sezione) {
+            // controlla se e' gia' presente il messaggio d'errore e lo rimuove
+            var parent = sezione.parentNode;
+            if (parent.children.length == 2) {
+                parent.removeChild(parent.children[1]);
+            }
+
+            corretto = sezioni[key];
+            var elemento = document.createElement("p");
+            elemento.className = "messaggi";
+            if(!sezioni[key])
+                elemento.appendChild(document.createTextNode("✘ " + spuntabili[key][1]));
+            else
+                elemento.appendChild(document.createTextNode("✔"));
+            var p = sezione.parentNode;
+            p.appendChild(elemento);
+        }
+    }
+    return corretto;
+}
+
+var spuntabili = { 
+    "genere[]" : ["labelGenere", "Selezionare almeno un genere"],
+    "console[]" : ["labelConsole", "Selezionare almeno una console"],
+    "tipologia" : ["labelTipologia", "Selezionare una tipologia"]
+};
+
+// Controllo che i filtri degli anni includano un intervallo temporale valido
+function checkChecked() {
+    var listaInput = document.getElementsByTagName('input');
+    var sezioni = new Array();
+    for (var key in spuntabili) {
+        var spuntato = false;
+        var i = 0;
+        while (!spuntato && i < listaInput.length) {
+            if (listaInput[i].name === key && listaInput[i].checked)
+                spuntato = true;
+            i++;
+        }
+        sezioni[key] = spuntato;
+    }
+    var tmp = mostraMessaggioCheckbox(sezioni);
+    return tmp;
+}
+//listaInput[i].type === spuntabili[key][0] && 
+
+function checkPassword() {
+    var pw = document.getElementById("password").value;
+    if(pw) {
+        var pw2 = document.getElementById("repeatpassword");
+        if (pw == pw2.value)
+            return mostraMessaggio(pw2,true);
+        else
+            return mostraMessaggio(pw2,false);
+    }
 }
 
 // Fa comparire il campo "gioco" se viene selezionata la categoria giochi in FormNotizie
 function handleClick() {
-
     var radio = document.getElementById("handler");
     var barraDiRicerca = document.getElementById("cercaTitolo");
-    if (radio) {
+    if (radio) 
         radio.onchange = function(event) {
             if(event.target.value == "Giochi")
                 barraDiRicerca.className = "visibile";
             else
                 barraDiRicerca.className = "invisibile";
         }
-    }
 }
 
-function checkPasswordRipetuta(pw1, pw2) {
-    if (pw2.value) {
-      if (!(password.value == rpassword.value)) {
-        setErrorBox(rpassword);
-        return false;
-      }
-    }
-    removeErrorBox(rpassword);
-    return true;
-}
+/******************* Script presenti in tutte le pagine *******************/
 
 /* Controlla che l'input della searchbar non sia vuoto o composto da spazi */
 function checkNotEmpty() {
@@ -140,7 +181,7 @@ function checkNotEmpty() {
         return true;
 }
 
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+/* Bottone menu a tendina */
 function responsiveMenu() {
     var x = document.getElementById("menu");
     if (x.className === "topnav") {
@@ -150,7 +191,7 @@ function responsiveMenu() {
     }
 }
 
-//script dalla pagina giochi//
+/******************* Script dalla pagina giochi *******************/
 
 var filtri = {
     "btn_console" : "console",
@@ -159,10 +200,10 @@ var filtri = {
     "btn_a" : "annofin"
 };
 
+// Gestisce l'apertura delle tendine dei filtri all'evento onclick
 function preparaFiltri() {
-
     var handler = document.getElementsByClassName("container");
-    if(handler) {
+    if(handler)
         handler[0].onclick = function(event) {
             var open = document.getElementsByClassName("dropdown-filter show");
             for (var i = 0; i < open.length; i++) {
@@ -170,26 +211,27 @@ function preparaFiltri() {
             }
             document.getElementById(filtri[event.target.id]).classList.toggle("show");
         }
-    }
 }
 
+// Controllo che i filtri degli anni includano un intervallo temporale valido
 function checkAnni() {
-
     var radios = document.getElementsByTagName('input');
     var anni = new Array();
-
     for (var i = 0; i < radios.length; i++)
         if (radios[i].type === 'radio' && radios[i].checked) {
             anni.push(radios[i].value);       
         }
+
+    var p = document.getElementById("categorie");
+    if (p.children.length == 3) {
+        p.removeChild(p.children[2]);
+    }
 
     if(anni.length == 2)
         if(anni[0] > anni[1]) {
             var elemento = document.createElement("div");
             elemento.className = "erroriFiltri";
             elemento.appendChild(document.createTextNode("Intervallo temporale sbagliato"));
-        
-            var p = document.getElementById("categorie");
             p.appendChild(elemento);
             return false;
         }
