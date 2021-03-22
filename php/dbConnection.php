9d4602994126d333a8071fe09e61b7971034927c
Ivan <ivan.piacere@studenti.unipd.it>
2021-03-22 22:21:56 +0100

    disattivati alcuni echo

--- php/dbConnection.php
+++ php/dbConnection.php
@@ -32,9 +32,11 @@ class DBAccess {
 
     #la funzione getResult deve ricevere in input una stringa già sanificata (sanitized)
     #altrimenti la sicurezza può essere compromessa
-    public function getResult($query, $silent=false){
+    public function getResult($query, $silent = true){
         $querySelect ="$query";
-        echo "db query: ".$querySelect."<br/>";
+        if(!$silent){
+            echo "db query: ".$querySelect."<br/>";
+        }
         $queryResult = mysqli_query($this->connection, $querySelect);
         /*echo "query result: ".$queryResult;*/
         if($queryResult==true){
